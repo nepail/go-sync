af76d8a88bdbb6c8ab4ce8e0c5b2a04d6def8eab
nepail <kls18z@hotmail.com>
2022-12-16 23:11:29 +0800

    加入靜態路由及頁面

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
 	"net/http"
 	"os"
 	"os/signal"
@@ -12,31 +14,37 @@ import (
 	"github.com/zserge/lorca"
 )
 
+//go:embed frontend/dist/*
+var FS embed.FS
+
 func main() {
 	// chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 	// cmd := exec.Command(chromePath, "--app=https://www.google.com")
 	// cmd.Start()
 
 	go func() {
-		gin.SetMode(gin.ReleaseMode)
+		gin.SetMode(gin.DebugMode)
 		router := gin.Default()
-		router.GET("/", func(c *gin.Context) {
-			c.String(http.StatusOK, "<h1> Hello World </h1>")
-		})
+		// router.GET("/", func(c *gin.Context) {
+		// 	c.String(http.StatusOK, "<h1> Hello World </h1>")
+		// })
+		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		router.StaticFS("/static", http.FS(staticFiles))
 		router.Run(":8080")
 	}()
 
 	var ui lorca.UI
 	// ui, _ = lorca.New("http://google.com", "", 800, 600, "--disable-sync", "--disable-translate")
 	// localhost 不走代理，所以必須使用127.0.0.1
-	ui, _ = lorca.New("http://127.0.0.1:8080", "", 800, 600, "--disable-sync", "--disable-translate")
+	ui, _ = lorca.New("http://127.0.0.1:8080/static/index.html", "", 800, 600, "--disable-sync", "--disable-translate")
 	chSignal := make(chan os.Signal, 1)
-	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 
 	// select 監聽可讀可寫的chan 沒有值的話阻塞目前線程 隨機輪詢
 	select {
 	case <-ui.Done():
 	case <-chSignal:
+		// cmd.Process.Kill()
 	}
 
 	// select {}
@@ -44,4 +52,7 @@ func main() {
 	// 關閉主線程或ui，會主動退出
 	ui.Close()
 
+	// 等待命令
+	// <-chSignal
+	// cmd.Process.Kill()
 }
