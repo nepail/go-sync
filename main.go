e4e0f72547a52fc6cc9da010ab696c4dae03d38c
nepail <kls18z@hotmail.com>
2022-12-16 22:37:18 +0800

    引入 gin

--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"net/http"
 	"os"
 	"os/signal"
+
+	// "os/exec"
 	"syscall"
 
+	"github.com/gin-gonic/gin"
 	"github.com/zserge/lorca"
 )
 
 func main() {
+	// chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+	// cmd := exec.Command(chromePath, "--app=https://www.google.com")
+	// cmd.Start()
+
+	go func() {
+		gin.SetMode(gin.ReleaseMode)
+		router := gin.Default()
+		router.GET("/", func(c *gin.Context) {
+			c.String(http.StatusOK, "<h1> Hello World </h1>")
+		})
+		router.Run(":8080")
+	}()
+
 	var ui lorca.UI
-	ui, _ = lorca.New("http://google.com", "", 800, 600, "--disable-sync", "--disable-translate")
+	// ui, _ = lorca.New("http://google.com", "", 800, 600, "--disable-sync", "--disable-translate")
+	// localhost 不走代理，所以必須使用127.0.0.1
+	ui, _ = lorca.New("http://127.0.0.1:8080", "", 800, 600, "--disable-sync", "--disable-translate")
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
 
@@ -19,6 +38,8 @@ func main() {
 	case <-ui.Done():
 	case <-chSignal:
 	}
+
+	// select {}
 
 	// 關閉主線程或ui，會主動退出
 	ui.Close()
