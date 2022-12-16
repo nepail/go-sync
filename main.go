d5978beda6fb538d32bae60014804b490199dc70
PHOENIX-5\jim <jim@phoenix-5.com>
2022-12-16 17:57:01 +0800

    add channel

--- main.go
+++ main.go
@@ -10,13 +10,17 @@ import (
 
 func main() {
 	var ui lorca.UI
-	ui, _ = lorca.New("http://goog.com", "", 800, 600, "--disable-sync", "--disable-translate")
+	ui, _ = lorca.New("http://google.com", "", 800, 600, "--disable-sync", "--disable-translate")
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
+
+	// select 阻塞目前線程
 	select {
 	case <-ui.Done():
 	case <-chSignal:
 	}
+
+	// 關閉主線程或ui，會主動退出
 	ui.Close()
 
 }
