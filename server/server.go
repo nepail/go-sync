15628a010430c7bb8aeca6128185e8bde21d1631
nepail <kls18z@hotmail.com>
2023-01-18 15:52:33 +0800

    move controller to controller Dir

--- server/server.go
+++ server/server.go
@@ -3,18 +3,12 @@ package server
 import (
 	"embed"
 	"io/fs"
-	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
-	"os"
-	"path"
-	"path/filepath"
 	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
-	"github.com/skip2/go-qrcode"
+	c "github.com/nepail/go-sync/server/controller"
 )
 
 // 把指定目錄的檔案嵌入到exe
@@ -30,11 +24,11 @@ func Run() {
 	// 	c.String(http.StatusOK, "<h1> Hello World </h1>")
 	// })
 	staticFiles, _ := fs.Sub(FS, "frontend/dist")
-	router.POST("/api/v1/files", FilesController)
-	router.GET("/api/v1/qrcodes", QrcodeController)
-	router.GET("/uploads/:path", UploadsController)
-	router.GET("/api/v1/addresses", AddressesController)
-	router.POST("/api/v1/texts", TextsController)
+	router.POST("/api/v1/files", c.FilesController)
+	router.GET("/api/v1/qrcodes", c.QrcodeController)
+	router.GET("/uploads/:path", c.UploadsController)
+	router.GET("/api/v1/addresses", c.AddressesController)
+	router.POST("/api/v1/texts", c.TextsController)
 	router.StaticFS("/static", http.FS(staticFiles))
 	router.NoRoute(func(c *gin.Context) {
 		path := c.Request.URL.Path
@@ -55,111 +49,4 @@ func Run() {
 		}
 	})
 	router.Run(":" + port)
-}
-
-func TextsController(c *gin.Context) {
-
-	var json struct {
-		Raw string `json:"raw"`
-	}
-
-	if err := c.ShouldBindJSON(&json); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	} else {
-		exe, err := os.Executable()
-		if err != nil {
-			log.Fatal(err)
-		}
-		dir := filepath.Dir(exe)
-		if err != nil {
-			log.Fatal(err)
-		}
-		filename := uuid.New().String()
-		uploads := filepath.Join(dir, "uploads")
-		err = os.MkdirAll(uploads, os.ModePerm)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fullpath := path.Join("uploads", filename+".txt")
-		err = ioutil.WriteFile(filepath.Join(dir, fullpath), []byte(json.Raw), 0644)
-		if err != nil {
-			log.Fatal(err)
-		}
-		c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath})
-	}
-}
-
-func AddressesController(c *gin.Context) {
-	addrs, _ := net.InterfaceAddrs()
-	var result []string
-	for _, address := range addrs {
-		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				result = append(result, ipnet.IP.String())
-			}
-		}
-	}
-	c.JSON(http.StatusOK, gin.H{"addresses": result})
-}
-
-func GetUploadsDir() (uploads string) {
-	exe, err := os.Executable()
-	if err != nil {
-		log.Fatal(err)
-	}
-	dir := filepath.Dir(exe)
-	uploads = filepath.Join(dir, "uploads")
-	return
-}
-
-func UploadsController(c *gin.Context) {
-	if path := c.Param("path"); path != "" {
-		target := filepath.Join(GetUploadsDir(), path)
-		c.Header("Content-Description", "File Transfer")
-		c.Header("Content-Transfer-Encoding", "binary")
-		c.Header("Content-Disposition", "attachment; filename"+path)
-		c.Header("Content-Type", "application/octet-stream")
-		c.File(target)
-	} else {
-		c.Status(http.StatusNotFound)
-	}
-}
-
-func QrcodeController(c *gin.Context) {
-	if content := c.Query("content"); content != "" {
-		png, err := qrcode.Encode(content, qrcode.Medium, 256)
-		if err != nil {
-			log.Fatal(err)
-		}
-		c.Data(http.StatusOK, "imge/png", png)
-	} else {
-		c.Status(http.StatusBadRequest)
-	}
-}
-
-func FilesController(c *gin.Context) {
-	file, err := c.FormFile("raw")
-	if err != nil {
-		log.Fatal(err)
-	}
-	exe, err := os.Executable()
-	if err != nil {
-		log.Fatal(err)
-	}
-	dir := filepath.Dir(exe)
-	if err != nil {
-		log.Fatal(err)
-	}
-	filename := uuid.New().String()
-	uploads := filepath.Join(dir, "uploads")
-	err = os.MkdirAll(uploads, os.ModePerm)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fullpath := path.Join("uploads", filename+filepath.Ext(file.Filename))
-	fileErr := c.SaveUploadedFile(file, filepath.Join(dir, fullpath))
-	if fileErr != nil {
-		log.Fatal(fileErr)
-	}
-	c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath})
 }
