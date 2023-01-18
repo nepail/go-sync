41b9c96d3f9f9bfecda9250c30dc65d5d8c8f717
nepail <kls18z@hotmail.com>
2023-01-18 14:42:48 +0800

    add router FilesController

--- main.go
+++ main.go
@@ -41,6 +41,7 @@ func main() {
 		// 	c.String(http.StatusOK, "<h1> Hello World </h1>")
 		// })
 		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		router.POST("/api/v1/files", FilesController)
 		router.GET("/api/v1/qrcodes", QrcodeController)
 		router.GET("/uploads/:path", UploadsController)
 		router.GET("/api/v1/addresses", AddressesController)
@@ -170,4 +171,32 @@ func QrcodeController(c *gin.Context) {
 	} else {
 		c.Status(http.StatusBadRequest)
 	}
+}
+
+func FilesController(c *gin.Context) {
+	file, err := c.FormFile("raw")
+	if err != nil {
+		log.Fatal(err)
+	}
+	exe, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dir := filepath.Dir(exe)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filename := uuid.New().String()
+	uploads := filepath.Join(dir, "uploads")
+	err = os.MkdirAll(uploads, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fullpath := path.Join("uploads", filename+filepath.Ext(file.Filename))
+	fileErr := c.SaveUploadedFile(file, filepath.Join(dir, fullpath))
+	if fileErr != nil {
+		log.Fatal(fileErr)
+	}
+	c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath})
+
 }
