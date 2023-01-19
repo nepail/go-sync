d31def96dced1787fb2ba4a9d62f3be214de1193
nepail <kls18z@hotmail.com>
2023-01-19 11:08:31 +0800

    ctrl C and close Browser over together

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
 
@@ -15,41 +16,72 @@ func main() {
 	// cmd := exec.Command(chromePath, "--app=https://www.google.com")
 	// cmd.Start()
 
+	chChromeDie := make(chan struct{})
+	chBackendDie := make(chan struct{})
+
 	// 啟動 gin 服務
 	go server.Run()
+	go StartBrowser(chChromeDie, chBackendDie)
 
-	ui := StartBrowser()
+	// ui := StartBrowser()
 
 	// 監聽中斷訊號
 	chSignal := listenToInterrupt()
 
-	// select 監聽可讀可寫的chan 沒有值的話阻塞目前線程 隨機輪詢
 	// 等待中斷訊號
-	select {
-	case <-ui.Done():
-	case <-chSignal:
-		// cmd.Process.Kill()
+	for {
+		select {
+
+		case <-chSignal:
+			fmt.Println("--------cmd close -----------")
+			chBackendDie <- struct{}{}
+
+		case <-chChromeDie:
+			fmt.Println("--------chrome close --------")
+			os.Exit(0)
+
+		}
 	}
 
-	// select {}
-
 	// 關閉主線程或ui，會主動退出
-	ui.Close()
+	// ui.Close()
 
 	// 等待命令
 	// <-chSignal
 	// cmd.Process.Kill()
 }
 
-func StartBrowser() lorca.UI {
-	var ui lorca.UI
+// func StartBrowser() lorca.UI {
+// 	var ui lorca.UI
+// 	// ui, _ = lorca.New("http://google.com", "", 800, 600, "--disable-sync", "--disable-translate")
+// 	// ui, _ = lorca.New("https://term.ptt.cc", "", 800, 600, "--disable-sync", "--disable-translate")
+// 	// localhost 不走代理，所以必須使用127.0.0.1
+// 	ui, _ = lorca.New("http://localhost:"+config.GetPort()+"/static/index.html", "", 800, 600, "--disable-sync", "--disable-translate")
+// 	return ui
+// }
+
+func StartBrowser(chChromeDie chan struct{}, chBackendDie chan struct{}) {
+
 	// ui, _ = lorca.New("http://google.com", "", 800, 600, "--disable-sync", "--disable-translate")
 	// ui, _ = lorca.New("https://term.ptt.cc", "", 800, 600, "--disable-sync", "--disable-translate")
 	// localhost 不走代理，所以必須使用127.0.0.1
+	// ui, _ = lorca.New("http://localhost:"+config.GetPort()+"/static/index.html", "", 800, 600, "--disable-sync", "--disable-translate")
+
+	var ui lorca.UI
 	ui, _ = lorca.New("http://localhost:"+config.GetPort()+"/static/index.html", "", 800, 600, "--disable-sync", "--disable-translate")
-	return ui
+
+	for {
+		select {
+		case <-ui.Done():
+			chChromeDie <- struct{}{}
+		case <-chBackendDie:
+			ui.Close()
+		}
+	}
+
 }
 
+// 監聽中斷訊息
 func listenToInterrupt() chan os.Signal {
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, os.Interrupt)
